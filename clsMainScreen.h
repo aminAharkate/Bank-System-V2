299419e439e87f63d3904290501b013b9878c2cc
aminAharkate <amineaharkate@gmail.com>
2025-10-27 18:41:04 +0100

    Add user management features and screens  - ManageUsersScreen: central hub for user operations - PrepareUserClass: define user data structure - ListUsersScreen: display all users - AddNewUserScreen: create new user accounts - DeleteUserScreen: remove users by username - UpdateUserScreen: modify existing user data - FindUserScreen: search and view user details

--- clsMainScreen.h
+++ clsMainScreen.h
@@ -7,6 +7,8 @@
 #include "clsDeleteClientScreen.h"
 #include "clsUpdateClientScreen.h"
 #include "clsFindClientScreen.h"
+#include "clsTransactionsScreen.h"
+#include "clsManageUsersScreen.h"
 
 using namespace std;
 
@@ -76,14 +78,15 @@ private:
 
     static void _ShowTransactionsMenue()
     {
-        cout << "\nTransactions Menue Will be here...\n";
+        // cout << "\nTransactions Menue Will be here...\n";
+        clsTransactionsScreen::ShowTransactionsMenue();
 
     }
 
     static void _ShowManageUsersMenue()
     {
-        cout << "\nUsers Menue Will be here...\n";
-
+        // cout << "\nUsers Menue Will be here...\n";
+        clsManageUsersScreen::ShowManageUsersMenue();
     }
 
     static void _ShowEndScreen()
@@ -130,11 +133,13 @@ private:
         case enMainMenueOptions::eShowTransactionsMenue:
             system("cls");
             _ShowTransactionsMenue();
+            _GoBackToMainMenue();
             break;
 
         case enMainMenueOptions::eManageUsers:
             system("cls");
             _ShowManageUsersMenue();
+            _GoBackToMainMenue();
             break;
 
         case enMainMenueOptions::eExit:
