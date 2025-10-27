6ea1026fde6509f226a0676c4049b8aedbda9ac6
aminAharkate <amineaharkate@gmail.com>
2025-10-27 21:12:04 +0100

    add more than 10 Extension

--- clsScreen.h
+++ clsScreen.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <iostream>
-
+#include "clsUser.h"
+#include "Global.h"
+#include "clsDate.h"
 
 using namespace std;
 
@@ -15,10 +17,28 @@ protected:
         {
             cout << "\n\t\t\t\t\t  " << SubTitle;
         }
-        cout << "\n\t\t\t\t\t______________________________________\n\n";
+        cout << "\n\t\t\t\t\t______________________________________\n";
+        cout << "\n\t\t\t\t\tUser: " << CurrentUser.UserName << "\n";
+        cout << "\t\t\t\t\tDate: " << clsDate::DateToString(clsDate())
+            << "\n\n";
+
     }
 
+    static bool CheckAccessRights(clsUser::enPermissions Permission)
+    {
+
+        if (!CurrentUser.CheckAccessPermission(Permission))
+        {
+            cout << "\t\t\t\t\t______________________________________";
+            cout << "\n\n\t\t\t\t\t  Access Denied! Contact your Admin.";
+            cout << "\n\t\t\t\t\t______________________________________\n\n";
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+
+    }
 
 };
-
-
